01a619011431050fc12216271c3784c02e05e1ec
musgravejw <musgravejw@gmail.com>
2020-08-21 22:29:04 -0400

    Implement Connection Pooling for multiple HTTP connections

--- src/server.c
+++ src/server.c
@@ -35,8 +35,19 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #include "parser.c"
+
+
+#define CLIENT_COUNT 10000
+
+struct client {
+  struct sockaddr_in address;
+  socklen_t addr_len;
+  int socket;
+};
+
 
 int main() {
   struct sockaddr_in server_address;
@@ -45,6 +56,8 @@ int main() {
   int enable = 1;
   ssize_t bytes_read;
   char *buffer;
+  struct client clients[CLIENT_COUNT] = { 0 };
+  size_t client_count = 0;
 
   printf("\nStarting server...");
 
@@ -70,49 +83,77 @@ int main() {
 
   // listen on port 80
   while(1) {
-    Request *request = malloc(sizeof(Request));
-    Response *response = malloc(sizeof(Response));
+    if (client_count < sizeof clients / sizeof *clients) {
+      struct sockaddr_in client_address;
+      socklen_t client_socklen = sizeof(client_address);
+      int client_socket = accept(server_socket, (struct sockaddr*) &client_address, &client_socklen);
 
-    new_request(request);
-    new_response(response);
+      if (client_socket != -1) {
+        fcntl(client_socket, F_SETFL, fcntl(client_socket, F_GETFL, 0) | O_NONBLOCK);
+        clients[client_count++] = (struct client) { .address = client_address, .addr_len = client_socklen, .socket = client_socket };
 
-    struct sockaddr_in client_address;
-    socklen_t client_socklen = sizeof(client_address);
-    int client_socket = accept(server_socket, (struct sockaddr*) 
-    	                       &client_address, &client_socklen);
-
-    // read into buffer
-    while(read(client_socket, buffer, 1028) > 0) {
-      // printf("\nReceived:\n%s\n", buffer);
-
-      parse_request(request, buffer);
-      log_request(request);
-
-      if (strcmp(request->method, "GET") == 0) {
-        build_response(request, response);
-        log_response(response);
-
-        char *res = response_to_string(response);
-        printf("\nSending: %s\n", res);
-
-        // return response
-        int n = write(client_socket, res, 1028);
-        if (n < 0) printf("Error writing to socket.");
-    } else if(strcmp(request->method, "HEAD") == 0
-        || strcmp(request->method, "POST") == 0
-        || strcmp(request->method, "PUT") == 0
-        || strcmp(request->method, "DELETE") == 0
-        || strcmp(request->method, "TRACE") == 0
-        || strcmp(request->method, "OPTIONS") == 0
-        || strcmp(request->method, "PATCH") == 0) {
-      printf("\nrequest: %s\n\n", request->method);
-    } else {
-      // free_request(request);
-      // free_response(response);
+        printf("\nNew client connection: %d", client_count);                                                         
+      }
     }
-  }
 
-    close(client_socket);
+    for (int i = 0; i < client_count; i++) {
+      // ssize_t bytes_read = read(clients[i].socket, buffer, 1028);
+      // printf("\nReceived: %s", buffer);
+
+      Request *request = malloc(sizeof(Request));
+      Response *response = malloc(sizeof(Response));
+
+      new_request(request);
+      new_response(response);
+
+      // struct sockaddr_in client_address;
+      // socklen_t client_socklen = sizeof(client_address);
+      // int client_socket = accept(server_socket, (struct sockaddr*) 
+      //                          &client_address, &client_socklen);
+
+      // read into buffer
+      while(read(clients[i].socket, buffer, 1028) > 0) {
+        // printf("\nReceived:\n%s\n", buffer);
+
+        parse_request(request, buffer);
+        log_request(request);
+
+        if (strcmp(request->method, "GET") == 0) {
+          build_response(request, response);
+          log_response(response);
+
+          char *res = response_to_string(response);
+          printf("\nSending: %s\n", res);
+
+          // return response
+        int n = write(clients[i].socket, res, 1028);
+
+        if (n < 0) {
+          printf("Error writing to socket.");
+        } else if(strcmp(request->method, "HEAD") == 0
+            || strcmp(request->method, "POST") == 0
+            || strcmp(request->method, "PUT") == 0
+            || strcmp(request->method, "DELETE") == 0
+            || strcmp(request->method, "TRACE") == 0
+            || strcmp(request->method, "OPTIONS") == 0
+            || strcmp(request->method, "PATCH") == 0) {
+          printf("\nrequest: %s\n\n", request->method);
+        } else {
+          // free_request(request);
+          // free_response(response);
+        }
+      }
+
+      if (bytes_read == 0) {
+        close(clients[i].socket);
+        client_count--;
+        memmove(clients + i, clients + i + 1, (client_count - i) * sizeof clients);
+        continue;
+      }
+    }
+
+      sleep(0);
+    }
   }
 
   return -1;
